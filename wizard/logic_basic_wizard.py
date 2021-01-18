e45e811e9a5c76fe32e5898e0d33000f55b1c571
Esteban Gomez <estebangomezhachy@gmail.com>
2021-01-18 17:13:19 -0400

    Arreglando pep

--- wizard/logic_basic_wizard.py
+++ wizard/logic_basic_wizard.py
@@ -16,10 +16,17 @@ class MathWizard(models.TransientModel):
     @api.depends("state_wizard_logic")
     def _sum_binary(self):
         logicbasic = self.env["logic.basic"]
-        logic_basic_check_ids = logicbasic.search([('state', '=', 'to_verify')])
+        logic_basic_check_ids = logicbasic.search([('state',
+                                                    '=',
+                                                    'to_verify'
+                                                    )])
         logic_basic_verify_ids = logicbasic.search([('state', '=', 'checked')])
         for record in self:
             if record.state_wizard_logic == 'to_verify':
-                record.spot_result = sum(logic_basic_verify_ids.mapped("binary_integer_number"))
+                record.spot_result = sum(
+                    logic_basic_verify_ids.mapped("binary_integer_number")
+                )
             else:
-                record.spot_result = sum(logic_basic_check_ids.mapped("binary_integer_number"))
+                record.spot_result = sum(
+                    logic_basic_check_ids.mapped("binary_integer_number")
+                )
